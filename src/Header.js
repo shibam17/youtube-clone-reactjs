ccc8270c749dcc9e4979229d9bf08116358aaecd
shibam17 <shibam.dipu@gmail.com>
2021-04-24 12:02:23 +0530

    started

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+import { Menu } from "@material-ui/icons";
+import React from "react";
+
+function Header() {
+  return (
+    <div classNam='header'>
+      <h1>Hello head</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkwGPilAlv5xJ6vXeBZpjL4dexdgdA1uo",
+  authDomain: "clone-63a8a.firebaseapp.com",
+  projectId: "clone-63a8a",
+  storageBucket: "clone-63a8a.appspot.com",
+  messagingSenderId: "812257910754",
+  appId: "1:812257910754:web:767065d9ee792b8af323bb",
+  measurementId: "G-DF3CSW0EH5",
+};
